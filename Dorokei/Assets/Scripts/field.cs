40f9539ef8f2784776b8169dca89b238839851fa
G-TUNENEXTGEAR-\takoi <tako.ishihata@gmail.com>
2019-11-02 16:28:37 +0900

    prefabのメタファイルをあげます

--- Dorokei/Assets/Scripts/field.cs
+++ Dorokei/Assets/Scripts/field.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class field : MonoBehaviour
 {
+    [Header("床の数")]
+    [SerializeField]
+    int TileMax = 19;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject obj = (GameObject)Resources.Load("Prefabs/Tile");
+
+        for(int i=0; i<TileMax;++i)
+        {
+            for(int j=0; j<TileMax; ++j)
+            {
+                Instantiate(obj, new Vector3(i * 1.0f, 0.0f, j * (-1.0f)), Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
