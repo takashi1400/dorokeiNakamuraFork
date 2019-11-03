2093f04e41049c71f3678dd0f72d72d00f35bc45
wiinaga <wiinaga.1005@gmail.com>
2019-11-03 14:55:35 +0900

    add_iroiro_nagai

--- Dorokei/Assets/Scripts/SceneMove.cs
+++ Dorokei/Assets/Scripts/SceneMove.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneMove : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class SceneMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKey)
+        {
+
+            SceneManager.LoadScene("Stage1");
+        }
     }
 }
