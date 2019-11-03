b52c2346fca53c3b87c796913fef5eea95156737
G-TUNENEXTGEAR-\takoi <tako.ishihata@gmail.com>
2019-11-03 11:53:10 +0900

    スタート位置反映

--- Dorokei/Assets/Scripts/GameControlManager.cs
+++ Dorokei/Assets/Scripts/GameControlManager.cs
@@ -25,6 +25,7 @@ public class GameControlManager : MonoBehaviour
     // フィールドデータ用
     public field FieldData;
     int StageNo;
+    float TotalTileLength;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +34,12 @@ public class GameControlManager : MonoBehaviour
         InputGameTimer = 0.0f;
         GameInputCounter = 0;
         StageNo = 0;
+        TotalTileLength = FieldData.TileLength * FieldData.StageTileMax[StageNo];
+
+
+        Vector3 posTheif = new Vector3(3, 0, 0);
+        Vector3 posPolice1 = new Vector3(2, 0, 0);
+        Vector3 posPolice2 = new Vector3(1, 0, 0);
 
         // 床並べる
         for (int i = 0; i < FieldData.TileMax; ++i)
@@ -40,12 +47,28 @@ public class GameControlManager : MonoBehaviour
             for (int j = 0; j < FieldData.TileMax; ++j)
             {
                 int data = FieldData.StageData[StageNo, i, j];
+                if( data == 1)
+                {
+                    posTheif = new Vector3(j * FieldData.TileLength - TotalTileLength / 2.0f,
+                            0.0f,
+                            i * (-1.0f) * FieldData.TileLength + TotalTileLength / 2.0f);
+                }else if( data == 2)
+                {
+                    posPolice1 = new Vector3(j * FieldData.TileLength - TotalTileLength / 2.0f,
+                            0.0f,
+                            i * (-1.0f) * FieldData.TileLength + TotalTileLength / 2.0f);
+                }else if( data == 3)
+                {
+                    posPolice2 = new Vector3(j * FieldData.TileLength - TotalTileLength / 2.0f,
+                            0.0f,
+                            i * (-1.0f) * FieldData.TileLength + TotalTileLength / 2.0f);
+                }
             }
         }
 
-        Instantiate(ObjTheif, new Vector3(3, 0, 0), Quaternion.identity);
-        Instantiate(ObjPolice1, new Vector3(2, 0, 0), Quaternion.identity);
-        Instantiate(ObjPolice2, new Vector3(1, 0, 0), Quaternion.identity);
+        Instantiate(ObjTheif, posTheif, Quaternion.identity);
+        Instantiate(ObjPolice1, posPolice1, Quaternion.identity);
+        Instantiate(ObjPolice2, posPolice2, Quaternion.identity);
     }
 
     // Update is called once per frame
