6283d9f28df2e71e39e4cf9e40e75edac55454db
takashi1400 <the.timewaster.may.be.me@gmail.com>
2019-11-03 15:16:05 +0900

    ライトっぽさをだした

--- Dorokei/Assets/CursorTurnAround.cs
+++ Dorokei/Assets/CursorTurnAround.cs
@@ -13,7 +13,7 @@ public class CursorTurnAround : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         // transformを取得
         Transform myTransform = this.transform;
@@ -21,7 +21,7 @@ public class CursorTurnAround : MonoBehaviour
         // ワールド座標を基準に、回転を取得
         Vector3 worldAngle = myTransform.eulerAngles;
         //worldAngle.x = worldAngle.x; // ワールド座標を基準に、x軸を軸にした回転を10度に変更
-        worldAngle.y += Time.deltaTime; // ワールド座標を基準に、y軸を軸にした回転を10度に変更
+        worldAngle.y += Time.deltaTime*100; // ワールド座標を基準に、y軸を軸にした回転を10度に変更
         //worldAngle.z = worldAngle.z; // ワールド座標を基準に、z軸を軸にした回転を10度に変更
         myTransform.eulerAngles = worldAngle; // 回転角度を設定
     }
