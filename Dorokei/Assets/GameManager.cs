4631fc1d7c0c54856766f448f2811796c66257d8
G-TUNENEXTGEAR-\takoi <tako.ishihata@gmail.com>
2019-11-03 10:03:34 +0900

    GameManger 更新

--- Dorokei/Assets/GameManager.cs
+++ Dorokei/Assets/GameManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // public
+    [Header("ゲームタイマー")]
+    public float GameTimer;
+
+    [Header("入力回数")]
+    public int GameInputCounter;
+
+    [Header("入力の時間")]
+    [SerializeField]
+    float InputSpan = 2.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameTimer = 0.0f;
+        GameInputCounter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // count timer
+        GameTimer += Time.deltaTime;
     }
 }
